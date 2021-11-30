bd3511e6f47b3aaabbf54899636fb504b5ee68b6
bruno <marqsbruno@gmail.com>
2021-11-30 16:08:08 -0300

    exercicios 1 e 2

---
+++ dia-3-javascript-logica-de-programacao-algoritmos/exercicios.js
@@ -0,0 +1,11 @@
+
+let n = 5;
+let linha = '';
+
+/* for (index = 0; index < n; index +=1){
+    linha += '*'
+} */
+for (index = 0; index < n; index +=1){
+    linha += '*'
+    console.log(linha)
+}
