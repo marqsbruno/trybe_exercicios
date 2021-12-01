008534578fea39e84ef72fddaf19b362af7848a8
bruno <marqsbruno@gmail.com>
2021-12-01 18:52:26 -0300

    exercicios

--- dia-3-javascript-logica-de-programacao-algoritmos/exercicios.js
+++ dia-3-javascript-logica-de-programacao-algoritmos/exercicios.js
@@ -1,11 +1,14 @@
+/* criar a variavel n
+criar a variavel de asteriscos
+imprimir n de asteristicos
+*/
 
 let n = 5;
 let linha = '';
 
-/* for (index = 0; index < n; index +=1){
-    linha += '*'
-} */
 for (index = 0; index < n; index +=1){
     linha += '*'
+}
+for (index = 0; index <n; index +=1){
     console.log(linha)
 }
